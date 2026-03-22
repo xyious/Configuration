e219eb83bbf4be03a99a6fe833a512d21a3c4160
Alexandra Knopf <xyious@gmail.com>
2026-03-21 21:05:17 -0700

    make nvim background transparent

--- .config/nvim/lua/xyious/plugins/color.lua
+++ .config/nvim/lua/xyious/plugins/color.lua
@@ -4,7 +4,7 @@ function ColorMyPencils(color)
 
 	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
 	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-
+    vim.g.mgz_disable_background = true
 end
 
 return {
